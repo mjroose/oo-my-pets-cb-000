2cdcc1e00d5a2a123d5a7e5f334dcad89bd08aa8
Michael Roose <mjroose@gmail.com>
2018-08-13 18:28:19 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -28,38 +28,43 @@ class Owner
 
   def buy_fish(name)
     fish = Fish.new(name)
-    @pets[:fishes] << fish
+    self.pets[:fishes] << fish
   end
 
   def buy_cat(name)
     cat = Cat.new(name)
-    @pets[:cats] << cat
+    self.pets[:cats] << cat
   end
 
   def buy_dog(name)
     dog = Dog.new(name)
-    @pets[:dogs] << dog
+    self.pets[:dogs] << dog
   end
 
   def walk_dogs
-    @pets[:dogs].each do |dog|
+    self.pets[:dogs].each do |dog|
       dog.mood = "happy"
     end
   end
 
   def play_with_cats
-    @pets[:cats].each do |cat|
+    self.pets[:cats].each do |cat|
       cat.mood = "happy"
     end
   end
 
   def feed_fish
-    @pets[:fishes].each do |fish|
+    self.pets[:fishes].each do |fish|
       fish.mood = "happy"
     end
   end
 
   def sell_pets
-
+    self.pets.each do |species|
+      species.each do |pet|
+        pet.mood = "nervous"
+      end
+    end
+    self.pets.clear
   end
 end
